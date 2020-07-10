71dc5a3b51e7b5d7dd7d8cbd1ef1c25bf84ae078
sugeng04.as <sugeng04.as@gmail.com>
2020-07-10 19:07:34 +0700

    ini templateku

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,18 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                {{-- Header --}}
+                <div class="card-header" style="text-align: center">Ini Judul</div>
 
+                {{-- Body --}}
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    
+                    ini body
 
-                    {{ __('You are logged in!') }}
                 </div>
+
             </div>
         </div>
     </div>
